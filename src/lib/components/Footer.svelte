db5cedaff24e63a4220ff5520a48475cf5e939d9
Luis/ <82966665+DEVLuisz@users.noreply.github.com>
2022-09-07 22:29:19 +0000

    Add files via upload

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer>
+  <img src="/icones/registered.png" alt="Icon Footer" class="registered" />
+  CopyRight | Created by &copy;Luís.jsx | Alura Cook - 2022
+</footer>
+
+<style>
+  footer {
+    background: url("/imagens/footer.png");
+    height: 100px;
+    font-weight: 700;
+    color: var(--branco);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .registered {
+    margin-right: 1px;
+  }
+</style>
